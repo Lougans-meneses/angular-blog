51f4a44d872544487542fd983783c5e8f6857d4e
lougans meneses <lougans_meneses@hotmail.com>
2024-01-16 17:23:14 -0300

    propriedade dinâmicas dos componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2EAi2LWjcsP5festROGoDWT70_VicTt7bbg&usqp=CAU';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
